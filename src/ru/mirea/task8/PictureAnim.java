30dace9e5cd1fd31ce6a798f94d79e32bcc129ae
NightDreamer <a.taraskin02@gmail.com>
2021-10-15 21:36:54 +0300

    Rev. 8.3

--- src/ru/mirea/task8/PictureAnim.java
+++ src/ru/mirea/task8/PictureAnim.java
@@ -11,19 +11,19 @@ public class PictureAnim {
 
         for (int i = 0; i < 8; i++)
         {
-            path[i] = scan.nextLine();
+            path[i] = scan.nextLine();      //тут я храню пути к кадрам анимации
         }
 
-        while(true) {
-            for (int i = 0; i < path.length; i++) {
-                ImageIcon img = new ImageIcon(path[i]); //создаём объект, хранящий картинку
+        while(true) {       //анимация будет идти, пока пользователь не закроет окно
+            for (int i = 0; i < path.length; i++) {     //проходим по всем кадрам
+                ImageIcon img = new ImageIcon(path[i]); //создаём объект, хранящий картинку и передаём путь как параметр
                 JLabel label = new JLabel(img);     //создаем лейбл c картинкой
                 frame.add(label); //добавляем лейбл с картинкой к окну
-                frame.setVisible(true);
-                try {                               //усыпляем программу на некоторое время
+                frame.setVisible(true); //показываем окно на мониторе
+                try {                               //усыпляем программу на некоторое время (какая-то устойчивая конструкция)
                     Thread.sleep(100);
                 } catch(InterruptedException ex) {}
-                frame.getContentPane().removeAll(); //извлекаем слой панели содержимого и очищаем его
+                frame.getContentPane().removeAll(); //извлекаем слой панели содержимого и очищаем его (проще говоря подготавливаем окно к следующему кадру)
             }
         }
     }
