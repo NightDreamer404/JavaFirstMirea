52d93bb1627a10cd54ce7625dbcb99e836921722
Artyom Taraskin <90523313+NightDreamer404@users.noreply.github.com>
2021-12-18 23:38:44 +0300

    Update CircleTest.java

--- src/ru/mirea/task3/CircleTest.java
+++ src/ru/mirea/task3/CircleTest.java
@@ -1,4 +1,11 @@
 package ru.mirea.task3;
 
 public class CircleTest {
+      public static void main (String[] args) {
+        Circle circle = new Circle("blue", 25);
+        System.out.println(circle);
+        circle.setColor("cyan");
+        circle.setRadius(12);
+        System.out.println(circle);
+    }
 }
